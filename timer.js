b1048f14031bf63490dc0590add61c57db665fa3
YgkSoldier <94282208+YgkSoldier@users.noreply.github.com>
2021-11-14 14:21:51 +0545

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,8 +1,8 @@
 function countdown() {
     var i = document.getElementById('counter');
     if (parseInt(i.innerHTML)<=0) {
-        location.href = '#clickimage';
+        location.href = '#link';
     }
     i.innerHTML = parseInt(i.innerHTML)-2;
 }
-setInterval(function(){ countdown(); },1100);
+setInterval(function(){ countdown(); },1200);
